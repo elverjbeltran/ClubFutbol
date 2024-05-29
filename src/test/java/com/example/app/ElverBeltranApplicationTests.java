b0d3468926ff11ceb43fbe96330809045b4a8cd2
elverjbeltran <elverbeltran04@gmail.com>
2024-05-29 00:20:15 -0500

    Club

--- src/test/java/com/example/app/ElverBeltranApplicationTests.java
+++ src/test/java/com/example/app/ElverBeltranApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ClubFutbolApplication.class) // Focaliza en la clase principal correcta
 public class ElverBeltranApplicationTests {
 
     @Test
     void contextLoads() {
     }
 }
+
 
